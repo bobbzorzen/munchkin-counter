1bb0c7d7189b31495eb901791312a392a892ba2d
Bobbzorzen <bobbzorzen@gmail.com>
2015-09-27 19:33:12 +0200

    Fixed some javascript parts

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,35 @@
 (function () {
     'use strict';
+    var Game = {
+        players: [],
 
+        init: function () {
+            console.log("INIT");
+        },
+
+        addPlayer: function (name) {
+            console.log("Name: ", name);
+            this.players.push(name);
+        },
+
+        printPlayers: function () {
+            console.log(this.players);
+        }
+
+    };
+    console.log("GAME: ", Game.init());
     $( document ).ready( function () {
-        Game.init("World");
+        Game.init();
+        Game.addPlayer("potato");
+        Game.addPlayer("potato2");
+        Game.printPlayers();
+
+        $(".level-modifier").on("click", function() {Game.printPlayers();});
     });
 
-    var test = function () {
-        console.log("Hello ")
-    };
-
-    var echo = function (string) {
-        test();
-        console.log(string);
-    };
-
-    window.Game = {
-        "init": echo
-    };
+    // window.Game = {
+    //     "init": Game.init,
+    //     "addPlayer": Game.addPlayer,
+    // };
     console.log("Game ready!");
 }());
